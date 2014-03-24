9951a59d0f410662b27a6a5106d26150f2d065a2
solygen <frank@solygen.de>
2014-03-24 07:14:40 +0100

    introduce battle values

--- src/js/character/main.js
+++ src/js/character/main.js
@@ -69,13 +69,61 @@ define('character/main', ['character/model', 'character/view'], function (model,
         //register save
         item.on('change', function () {
             console.warn('Save');
-            item.save(feedback, options);
+            //item.save(feedback, options);
         });
 
         //create/draw view
         itemView = view.create({
             model: item
         });
+
+
+        var change = function (id, value) {
+            var node = $('#' + id),
+                fieldval = $('#' + id).text(),
+                current = parseFloat(fieldval === '' ? 0 : fieldval, 10),
+                color = current > value ? 'red' : 'green';
+            node.text(value).finish();
+            //animate
+            if (current !== value && current !== 0) {
+                node.css('color', color)
+                    .animate({fontSize: '20px'})
+                    .animate({fontSize: '12px'})
+                    .animate({fontSize: '16px'})
+                    .animate({fontSize: '16px'}, function () {
+                        $(this).css('color', 'black');
+                    });
+            }
+        };
+
+        var calc = function () {
+            var hp = item.get('body') + item.get('constituation') + 10;
+            change('hit-points', hp);
+
+            var defense = item.get('body') + item.get('constituation');
+            change('defense', defense);
+
+            var initiative = item.get('agility') + item.get('mobility');
+            change('initiative', initiative);
+
+            var mr = (item.get('mobility') / 2) + 1;
+            change('movement-rate', mr);
+
+            var melee = item.get('body') + item.get('strength');
+            change('melee-attack', melee);
+
+            var ranged = item.get('body') + item.get('dexterty');
+            change('ranged-attack', ranged);
+
+            var spellcasting = item.get('mind') + item.get('aura');
+            change('spellcasting', spellcasting);
+
+            var ts = item.get('body') + item.get('dexterty');
+            change('targeted-spellcasting', ts);
+        };
+
+        item.on('change', calc);
+        calc();
 
         itemView.render();
     };
