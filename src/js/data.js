c6fda3bbe8cbc7b7acf012915790e723922b0611
solygen <account@solygen.de>
2014-04-18 13:25:21 +0300

    use local data as fallback

--- src/js/data.js
+++ src/js/data.js
@@ -2,16 +2,24 @@ define('data', [], function () {
 
     'use strict';
 
-    var BASE = 'https://rawgithub.com/DungeonSlayers/dungeonslayers.json/master/data-min/deu/',
+    var BASE = 'https://rawgit.com/DungeonSlayers/dungeonslayers.json/master/data-min/deu/',
+        BASE_FALLBACK = 'data/',
         data = {},
-        get = function (id) {
-            var def = $.Deferred();
+        get = function (id, base) {
+            var def = $.Deferred(),
+                base = base || BASE;
 
             if (!data[id]) {
                 //server
                 $.getJSON(BASE + id + '.min.json', function (response) {
                     data[id] = response[id] ? response[id] : response;
                     def.resolve(data[id]);
+                }).then(undefined, function () {
+                    //fallback 
+                    $.getJSON(BASE_FALLBACK + id + '.min.json', function (response) {
+                        data[id] = response[id] ? response[id] : response;
+                        def.resolve(data[id]);
+                    });
                 });
             } else {
                 //var
